ce81222f8a6f4f417ff852862efcd56ad1a1fee7
AndreyMikhailov <mands315@gmail.com>
2023-05-03 22:45:55 +0300

    UI complete

--- ByteCoin/Resources.swift
+++ ByteCoin/Resources.swift
@@ -10,5 +10,11 @@ import UIKit
 enum Resources {
     enum Colors {
         static var backgroundColor = UIColor(named: "Background Color")
+        static var titleColor = UIColor(named: "Title Color")
+        static var iconColor = UIColor(named: "Icon Color")
+    }
+    
+    enum Icons {
+        static var bitcoinIcon = UIImage(systemName: "bitcoinsign.circle.fill")
     }
 }
